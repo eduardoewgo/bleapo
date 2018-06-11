6940c2f9404ae8cd63ae9b7dae3292f130ccc71d
Eduardo Godoy <eduardo.wickertg@gmail.com>
2018-06-11 16:51:52 -0300

    Firestore temporarily credentials

---
+++ src/environments/firestore.ts
@@ -0,0 +1,8 @@
+export const firestore = {
+    apiKey: "AIzaSyBxNYSU_ffq-tYneYphl0jv5k7ZpQKDB8Q",
+    authDomain: "bleapo-eg.firebaseapp.com",
+    databaseURL: "https://bleapo-eg.firebaseio.com",
+    projectId: "bleapo-eg",
+    storageBucket: "bleapo-eg.appspot.com",
+    messagingSenderId: "663316842543"
+};
