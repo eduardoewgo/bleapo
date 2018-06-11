e34e7050aff479d38c073ac62afb92c0dc5fafc2
Eduardo Godoy <eduardo.wickertg@gmail.com>
2018-06-11 16:18:18 -0300

    Init for auth

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,9 +1,28 @@
 import {Injectable} from '@angular/core';
+import {AngularFireAuth} from "angularfire2/auth";
+import {User} from "../../shared/interfaces/user";
 
 @Injectable()
 export class AuthProvider {
 
-    constructor() {
-        console.log('Hello AuthProvider Provider');
+    user: User;
+
+    constructor(private afAuth: AngularFireAuth) {
+    }
+
+    login(): Promise<User> {
+        return this.afAuth.auth.signInWithEmailAndPassword('jao@gmail.com', '123456')
+            .then((data) => this.user = {...data.user})
+            .catch((err: { code: string, message: string }) => console.warn(err));
+    }
+
+    logout(): Promise<User> {
+        return this.afAuth.auth.signOut()
+            .then(() => this.user = null)
+            .catch((err) => console.warn(err));
+    }
+
+    authenticated(): boolean {
+        return !!this.user;
     }
 }
