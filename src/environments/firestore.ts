d10714f41efe8c95b452d23df3328f079468ffd6
eduardoewgo <39170116+eduardoewgo@users.noreply.github.com>
2018-07-16 19:05:32 -0300

    Credentials removed and firestore project shut down.

--- src/environments/firestore.ts
+++ src/environments/firestore.ts
@@ -1,8 +1,3 @@
 export const firestore = {
-    apiKey: "AIzaSyBxNYSU_ffq-tYneYphl0jv5k7ZpQKDB8Q",
-    authDomain: "bleapo-eg.firebaseapp.com",
-    databaseURL: "https://bleapo-eg.firebaseio.com",
-    projectId: "bleapo-eg",
-    storageBucket: "bleapo-eg.appspot.com",
-    messagingSenderId: "663316842543"
+   
 };
